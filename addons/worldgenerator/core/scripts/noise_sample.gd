aeb3fe6b5f1c02ceb6c46a2681f3a1c631609ff9
jthorp07 <jackthorp07@gmail.com>
2025-05-01 13:15:41 -0400

    Fix wrapping algorithm

--- addons/worldgenerator/core/scripts/noise_sample.gd
+++ addons/worldgenerator/core/scripts/noise_sample.gd
@@ -17,25 +17,26 @@ func _init(width: int, height: int):
 		self._sample[i] = PackedFloat32Array(row)
 
 
-## Populates this sample by sampling 2D noise from `noise`
+## Populates this sample by sampling 2D noise from [param noise]
 func sample(noise: FastNoiseLite):
 	for y in range(self._height):
 		for x in range(self._width):
 			self._sample[y][x] = noise.get_noise_2d(x, y)
 
 
-## Populates this sample by sampling 3D noise from `noise` in using[br]
-## a cylindrical formula that results in seamless horizontal wrapping
+## Populates this sample by sampling 3D noise from [param noise] in such[br]
+## a way that an isometric sample is collected with the exception of the[br]
+## sample seamlessly wrapping on the Y axis (left to right)
 func sample_with_wrap(noise: FastNoiseLite):
 	var fwidth: float = float(self._width)
 	var fheight: float = float(self._height)
+	var radius: float = fwidth / TAU
 	for y in range(self._height):
 		for x in range(self._width):
-			# Sinusoidal functions with period=width and amplitude=width/2
-			# Makes the looper still move 1 on each axis for each sample value & wrap on one axis
-			var nx = fwidth / 2.0 * cos(TAU * float(x) / fwidth)
-			var ny = fwidth / 2.0 * sin(TAU * float(x) / fwidth)
-			var nz = y
+			var radians: float = TAU * float(x) / fwidth
+			var nx: float = radius * cos(radians)
+			var ny: float = radius * sin(radians)
+			var nz: float = y
 			self._sample[y][x] = noise.get_noise_3d(nx, ny, nz)
 
 
